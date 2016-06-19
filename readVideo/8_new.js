f79bbe74173c8e8b58a4fb79d1478b10c637e8e6
Jeff Chen <jeffc0216@gmail.com>
2016-06-19 11:33:20 +0800

    Add files via upload

---
+++ readVideo/8_new.js
@@ -0,0 +1,25 @@
+var file;
+var fileReader;
+
+function doFirst(){
+	document.getElementById('myFile').onchange = fileChange;
+	document.getElementById('openButton').onclick = readFile;
+}
+function fileChange(){
+	file = document.getElementById('myFile').files[0];
+	var message = 'File Name: ' + file.name+'\n';
+	message += 'File Size: ' + file.size+' byte(s)\n';
+	message += 'File Type: ' + file.type+'\n';
+	message += 'Last Modified: ' + file.lastModifiedDate+'\n';
+	document.getElementById('fileInfo').value = message;
+	fileReader = new FileReader();
+	fileReader.onload = function(){
+		var myMovie = document.getElementById('myMovie');
+		myMovie.src = fileReader.result;
+		myMovie.controls = true;
+	};
+}
+function readFile(){
+	fileReader.readAsDataURL(file);
+}
+window.addEventListener('load', doFirst, false);
